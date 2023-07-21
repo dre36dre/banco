0dbb89d4d954bc518fa14305aba92aecbe4a03fb
Anderson freires <dre.son36@gmail.com>
2023-07-21 17:52:39 -0300

    criaçao da classe conta

---
+++ src/banco/Conta.java
@@ -0,0 +1,8 @@
+package banco;
+
+public class Conta {
+ String titular;
+ int agencia;
+ double saldo ;
+ int numero;
+}
---
+++ src/banco/Principal.java
@@ -0,0 +1,14 @@
+package banco;
+
+public class Principal {
+public static void main(String[] args) {
+	Conta minhaConta=new Conta();
+	minhaConta.titular="anderson";
+	minhaConta.agencia=123;
+	minhaConta.numero=987;
+	minhaConta.saldo=10_000;
+	
+	System.out.println("Titular: "+minhaConta.titular);
+	System.out.println("Saldo: "+ minhaConta.saldo);
+}
+}
