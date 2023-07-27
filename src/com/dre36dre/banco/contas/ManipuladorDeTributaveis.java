5e8413df01baa60c7b79760e5a8c47514b9366f6
Anderson freires <dre.son36@gmail.com>
2023-07-27 16:13:28 -0300

    Criaçao de interfaces

--- src/com/dre36dre/banco/contas/ManipuladorDeTributaveis.java
+++ src/com/dre36dre/banco/contas/ManipuladorDeTributaveis.java
@@ -5,17 +5,18 @@ import com.dre36dre.banco.model.Tributavel;
 
 public class ManipuladorDeTributaveis {
 	private double total;
-	
-	public void  calculaImpostos(Evento evento) {
-	total=0;
-	int tamanho=evento.getTamanhoDaLista("listaTributaveis");
-	for(int i=0;i < tamanho; i++) {
-		Tributavel t=evento.getTributavel("listaTributavel",i);
-		total +=t.getValorTributavel();
+
+	public void calculaImpostos(Evento evento) {
+		total = 0;
+		int tamanho = evento.getTamanhoDaLista("listaTributaveis");
+		for (int i = 0; i < tamanho; i++) {
+			Tributavel t = evento.getTributavel("listaTributavel", i);
+			total += t.getValorTributavel();
+		}
 	}
-	}
+
 	public double getTotal() {
 		return total;
 	}
-	
+
 }
